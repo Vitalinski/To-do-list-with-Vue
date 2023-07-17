f0d5f7814a84efc3d2c879c86379f65de5a728fb
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2023-07-17 17:14:00 +0200

    Overlay + BEM
    Overlay + BEM

--- src/components/Initial.vue
+++ src/components/Initial.vue
@@ -1,24 +1,38 @@
 <template>
-    <div class="starter-text">
-      Let create some tasks!
-      <div class="add">
-        <button class="starter-add-btn" @click="showForm">Add new task</button>
-      </div>
-    </div>
+  <div class="initial">
+    <div class="initial__text">Let create some tasks!</div>
+
+    <button class="initial__btn" @click="showForm">Add new task</button>
+  </div>
 </template>
 
 <script>
 export default {
-name:'Initial',
-methods:{
+  name: "Initial",
+  methods: {
     showForm() {
       this.$emit("showForm");
     },
-}
-}
-
+  },
+};
 </script>
 
 <style>
-
+.initial {
+  text-align: center;
+}
+.initial__text {
+  color: #fff;
+  font-weight: bold;
+  font-size: 48px;
+  margin-top: 150px;
+}
+.initial__btn {
+  font-size: 24px;
+  font-weight: bold;
+  border-radius: 12px;
+  padding: 10px;
+  border: none;
+  margin-top: 50px;
+}
 </style>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,48 +1,46 @@
 <template>
   <div class="main-page">
-    <h1>To do list</h1>
-    <CreateForm v-model:isActive="isActive" @addTask="addTask"> </CreateForm>
-
-    <RedactForm
-      v-model:isRedactActive="isRedactActive"
+    <h1 class="main-page__title">To do list</h1>
+<OverlayForm
+v-model:isRedactActive="isRedactActive"
       @redactTask="redactTask"
       :currentTask="selectedTask"
-    >
-    </RedactForm>
+      v-model:isActive="isActive"
+       @addTask="addTask"
+       @hideForm="hideForm"
+      ></OverlayForm>
 
     <div v-if="tasks.length > 0">
       <div class="buttons">
-        <div class="add">
-          <button class="btn" @click="showForm">Add new task</button>
+        <div class="buttons__add">
+          <button class="buttons__btn btn" @click="showForm">Add new task</button>
         </div>
-        <div class="sort">
-          <button class="btn" @click="sortByDate">Sort by date</button>
-          <button class="btn" @click="sortByPriority">Sort by priority</button>
+        <div class="buttons__sort">
+          <button class="buttons__btn btn" @click="sortByDate">Sort by date</button>
+          <button class="buttons__btn btn" @click="sortByPriority">Sort by priority</button>
         </div>
       </div>
-      <TaskBoard
+      <TasksBoard
         :tasks="tasks"
         :priority="priority"
         @showRedactForm="showRedactForm"
         @removeTask="removeTask"
-      ></TaskBoard>
+      ></TasksBoard>
     </div>
     <Initial v-else @showForm="showForm"> </Initial>
   </div>
 </template>
 
 <script>
-import CreateForm from "../components/CreateForm.vue";
 import Initial from "../components/Initial.vue";
-import RedactForm from "../components/RedactForm.vue";
-import TaskBoard from "../components/TaskBoard.vue";
+import OverlayForm from '../components/OverlayForm.vue';
+import TasksBoard from "../components/TasksBoard.vue";
 export default {
   name: "MainPage",
   components: {
-    CreateForm,
-    RedactForm,
     Initial,
-    TaskBoard,
+    TasksBoard,
+    OverlayForm,
   },
   data() {
     return {
@@ -59,9 +57,15 @@ export default {
     };
   },
   methods: {
+    hideForm(){
+      this.isActive=false;
+      this.isRedactActive= false
+    },
     addTask(task) {
       this.tasks.push(task);
       this.isActive = false;
+      this.isRedactActive = false;
+
     },
     sortByPriority() {
       this.tasks.sort((a, b) => a.priority.localeCompare(b.priority));
@@ -77,6 +81,8 @@ export default {
           task.priority = redactedTask.priority;
         }
       }
+      this.isRedactActive = false
+      this.isActive= false 
     },
     removeTask(currentTask) {
       this.tasks = this.tasks.filter((task) => task.id !== currentTask.id);
@@ -85,6 +91,7 @@ export default {
       this.isActive = true;
     },
     showRedactForm(task) {
+      this.isActive=true
       this.isRedactActive = true;
       this.selectedTask.title = task.title;
       this.selectedTask.description = task.description;
@@ -112,22 +119,12 @@ export default {
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
+  
 }
-.starter-text {
-  color: #fff;
-  font-weight: bold;
-  font-size: 3em;
+.main-page__title {
   text-align: center;
-  margin-top: 3em;
 }
-.starter-add-btn {
-  font-size: 0.5em;
-  font-weight: bold;
-  border-radius: 12px;
-  padding: 0.4em;
-  border: none;
-  margin-top: 2em;
-}
+
 .buttons {
   margin-top: 1em;
   display: flex;
@@ -141,89 +138,10 @@ export default {
   border: none;
   font-size: 1em;
 }
-.sort .btn {
+.buttons__sort .btn {
   margin-left: 1em;
 }
-.task-description {
-  max-height: 3.2em;
-  overflow-wrap: break-word;
-  overflow: auto;
-}
-.task {
-  border: 1px solid #000;
-  margin-bottom: 10px;
-  background-color: #ffff;
-  border-radius: 4px;
-  padding: 0.2em;
-  border-left: 1em solid #000;
-  min-height: 8em;
-  display: grid;
-  grid-template: 0 auto 1fr auto / 1fr;
-}
-.task-field {
-  margin-top: 2em;
-  display: grid;
-  grid-template: 1fr / repeat(3, 30%);
-  grid-auto-flow: row;
-  justify-content: space-between;
-}
-.task-title {
-  text-align: center;
-  font-weight: bold;
-  margin-top: 1.5em;
-  overflow: auto;
-}
-.task-date {
-  font-size: 0.7em;
-  text-align: right;
-}
-h1 {
-  text-align: center;
-  color: black;
-  font-weight: bold;
-}
 
-.task-btns {
-  text-align: right;
-}
-.task-btns button {
-  width: 2em;
-  height: 1.5em;
-  border-color: white;
-  background-color: #fff;
-  margin-left: 0.2em;
-}
-button:hover {
-  cursor: pointer;
-}
-.remove-btn {
-  color: red;
-  font-weight: bold;
-}
-.remove-btn:hover {
-  background-color: red;
-  color: #fff;
-  border-color: red;
-}
-.redact-img {
-  max-height: 100%;
-  max-width: 100%;
-}
-.redact-btn:hover {
-  border-color: rgba(0, 0, 0, 0.5);
-}
-.task-list-enter-active,
-.task-list-leave-active {
-  transition: all 0.5s ease;
-}
-.task-list-enter-from,
-.task-list-leave-to {
-  opacity: 0;
-  transform: translateX(30px);
-}
-.task-list-move {
-  transition: transform 0.8s ease;
-}
 </style>
 
 
