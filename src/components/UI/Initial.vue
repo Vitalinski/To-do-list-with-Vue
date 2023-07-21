9dd62dcb07067b77375ef96cd7c5339d165674b5
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2023-07-21 16:51:43 +0200

    second version
    second version

---
+++ src/components/UI/Initial.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="initial">
+    <div class="initial__text">Let create some tasks!</div>
+
+    <button class="initial__btn " @click="showForm">Add new task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Initial",
+  methods: {
+    showForm() {
+      this.$emit("showForm");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.initial {
+  text-align: center;
+
+  &__text {
+  color: #fff;
+  font-weight: bold;
+  font-size: 48px;
+  margin-top: 150px;
+}
+&__btn {
+  font-size: 24px;
+  font-weight: bold;
+  border-radius: 12px;
+  padding: 10px;
+  border: none;
+  margin-top: 50px;
+
+  &:hover{
+  background-color: black;
+  color: white;
+}
+}
+}
+
+
+</style>
