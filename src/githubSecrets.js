84498882ba48df2eb713459c07e63e2fd3b48790
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2023-11-08 14:18:01 +0100

    setting up github workflow

--- src/githubSecrets.js
+++ src/githubSecrets.js
@@ -1,5 +1,29 @@
-import { getInput } from "@actions/core"
+import { getInput , setFailed } from "@actions/core"
+import { context, getOctokit } from "@actions/github"
 
+async function run(){
+    const token = getInput("gh-token");
+    const label = getInput("label")
+
+    const octokit = getOctokit(token)
+    const pullRequest = context.payload.pull_request;
+    try{
+        if(!pullRequest){
+            throw new Error("This action can only be run on Pull Request")
+
+        }
+
+        await octokit.rest.issues.addLabels({
+            owner:context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: (label)
+        });
+    } catch(error){
+        setFailed((error)?.message ?? "Unknown error")
+    }
+}
+run()
 
 const key = getInput('key')
 console.log(key )
