030ada049061b942e3f3c628f975c81b01cdf33a
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2023-12-02 17:19:47 +0100

    Updated base path to fix issues

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/Kanban-board-with-Vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
