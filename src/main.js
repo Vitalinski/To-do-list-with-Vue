8bb31600fa52156ee28d7f140461bd25725070fc
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2023-12-10 17:41:43 +0100

    Firebase error defined

--- src/main.js
+++ src/main.js
@@ -17,9 +17,9 @@ const firebaseConfig = {
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId:import.meta.env.VITE_FIREBASE_Measurement_ID , 
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID, 
   };
   for(let key in firebaseConfig){
     console.log(`${key} = ${firebaseConfig[key]}`)
