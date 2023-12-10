a68fa960c56a847e9a10dc816135af45c0a4f678
Vitalinski <126358082+Vitalinski@users.noreply.github.com>
2023-12-10 17:18:24 +0100

    Try to fix Firebase error

--- src/main.js
+++ src/main.js
@@ -21,6 +21,9 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
     measurementId:import.meta.env.VITE_FIREBASE_Measurement_ID , 
   };
+  for(key in firebaseConfig){
+    console.log(`${key} = ${firebaseConfig[key]}`)
+  }
   let vueApp
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
